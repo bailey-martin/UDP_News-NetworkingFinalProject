51538ed888ca21efb71cc30033a20463783e3956
Bailey Martin <baileymartin1999@yahoo.com>
2018-11-01 13:09:14 -0400

    Added Code to Main.java to execute the program

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
-
+public class Main{
+    public static void main(String[] args){
+        int port = 55555;
+        UDP_Server server = new UDP_Server (port);
+        UDP_Client client = new UDP_Client (port);
+        
+        ExecuteService executeTheService = Executors.newFixedThreadPoll(2);
+        executeTheService.submit(client);
+        executeTheService.submit(server);
+    }//end of main()
+}//end of Main class
