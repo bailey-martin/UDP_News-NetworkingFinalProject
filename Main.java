8085e83feeeffc98c78cf0b3cfa5223f39e7f323
ach10718 <44448511+ach10718@users.noreply.github.com>
2018-11-01 13:45:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
 public class Main{
     public static void main(String[] args){
-        int port = 55555;
+        //int port = 55555;
+        int port=50001;
         UDP_Server server = new UDP_Server (port);
         UDP_Client client = new UDP_Client (port);
         
-        ExecuteService executeTheService = Executors.newFixedThreadPoll(2);
+        ExecutorService executeTheService = Executors.newFixedThreadPool(2);
         executeTheService.submit(client);
         executeTheService.submit(server);
     }//end of main()
