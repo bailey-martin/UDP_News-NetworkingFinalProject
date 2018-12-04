38ad828520b736db207fbc8a3667b1dda02264b4
bailey-martin <44448861+bailey-martin@users.noreply.github.com>
2018-12-04 17:54:53 -0500

    Update IPServerSender.java

--- CSC360FinalProject/src/Interface/IPServerSender.java
+++ CSC360FinalProject/src/Interface/IPServerSender.java
@@ -28,16 +28,46 @@ public class IPServerSender {
         address = InetAddress.getByName("weberkcudafac");
     }
  
-    public String sendEcho(String msg) throws IOException {
-        buf = msg.getBytes();
-        DatagramPacket packet 
-          = new DatagramPacket(buf, buf.length, address, 55555);
-        socket.send(packet);
-        packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
-        String received = new String(
-          packet.getData(), 0, packet.getLength());
-        return received;
+    public void sendEcho(String msg) throws IOException {
+//        buf = msg.getBytes();
+//        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 55555);
+//        socket.send(packet);
+
+         InetAddress aHost = InetAddress.getByName("weberkcudafac");
+        (new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Please enter the news item that you wish to share:\n");
+                Scanner scan = new Scanner(System.in);
+                String str = scan.nextLine();
+                byte data[] = str.getBytes();
+                DatagramSocket socket = null;
+                try {
+                    socket = new DatagramSocket();
+                   // socket.setBroadcast(true);
+                } catch (SocketException ex) {
+                    ex.printStackTrace();
+                }//end of catch
+
+                DatagramPacket packet = new DatagramPacket(data, data.length, aHost, 55555);
+                int i = 0;
+                while (i < 10) { //begin of while
+                    try { //begin of try()
+                        System.out.println("Sending news item: " + new String(packet.getData()));
+                        socket.send(packet);
+                        Thread.sleep(50);
+                        i++;
+                        
+                    }//end of try
+                    catch (IOException ex) {
+                        ex.printStackTrace();
+                    }//end of catch
+                    catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }//end of catch
+                }//end of while-loop
+            }//end of run()
+        }).start(); //end of thread
     }
  
     public void close() {
