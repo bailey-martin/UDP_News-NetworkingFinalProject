ef7a1cdc08eece142d430e109f07dac8664a00cc
Matt McMinn <44448869+Matt-McMinn@users.noreply.github.com>
2018-12-04 18:10:12 -0500

    Datagram Test number change

--- CSC360FinalProject/src/Interface/SocketTest.java
+++ CSC360FinalProject/src/Interface/SocketTest.java
@@ -58,7 +58,7 @@ public class SocketTest {
 
                 DatagramPacket packet = new DatagramPacket(data, data.length, aHost, 55555);
                 int i = 0;
-                while (i < 10) { //begin of while
+                while (i < 5) { //begin of while
                     try { //begin of try()
                         System.out.println("Sending news item: " + new String(packet.getData()));
                         socket.send(packet);
