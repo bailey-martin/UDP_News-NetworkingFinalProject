1438e724fa7b3e1e7eaebb712e980c27409ae759
bailey-martin <44448861+bailey-martin@users.noreply.github.com>
2018-12-04 17:40:59 -0500

    Changed the server port to 55555

--- CSC360FinalProject/src/Interface/UDPServer.py
+++ CSC360FinalProject/src/Interface/UDPServer.py
@@ -1,5 +1,5 @@
 from socket import *
-serverPort = 12000
+serverPort = 55555
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
