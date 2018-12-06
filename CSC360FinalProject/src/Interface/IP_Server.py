8f97808fadb2bbc8697a99831132c6ef8d27959c
ach10718 <44448511+ach10718@users.noreply.github.com>
2018-12-06 15:50:39 -0500

    fixed

--- CSC360FinalProject/src/Interface/IP_Server.py
+++ CSC360FinalProject/src/Interface/IP_Server.py
@@ -4,18 +4,18 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 ip_addresses = []
-messages = "";
+messages = ""
+
+def printTheIPs():
+   global messages
+   for x in range(len(ip_addresses)):
+        messages += ip_addresses[x];
+		
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     print("Incoming message " + message.decode())
-    modifiedMessage = message.decode().upper()
-    ip_addresses.append(modifiedMessage)
-    print(modifiedMessage)
+    ip_addresses.append(clientAddress[0])
     printTheIPs()
-    for i in range (len(ip_addresses)):
-        serverSocket.sendTo(bytes(messages, 'utf-8'), (ip_addresses[i], 55555))
+    for i in range(0, len(ip_addresses)):
+        serverSocket.sendto(bytes(messages, 'utf-8'), (ip_addresses[i], 55555))
     #serverSocket.sendto(modifiedMessage.encode(), clientAddress)
-
-def printTheIPs():
-   for x in range(len(ip_addresses)):
-        messages += ip_addresses[x];
