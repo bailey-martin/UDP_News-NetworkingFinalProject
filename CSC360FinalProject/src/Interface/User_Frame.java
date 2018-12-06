3e0f140a505c0c54dcd0f8811ce2b6a8e9a3743a
Matt-McMinn <44448869+MattMC2020@users.noreply.github.com>
2018-12-06 16:18:09 -0500

    Import IO Exception fix

--- CSC360FinalProject/src/Interface/User_Frame.java
+++ CSC360FinalProject/src/Interface/User_Frame.java
@@ -174,7 +174,7 @@ public class User_Frame extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) throws UnknownHostException {
+    public static void main(String args[]) throws UnknownHostException, IOException {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
