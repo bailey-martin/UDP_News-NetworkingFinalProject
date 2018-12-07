90396430159e44322558f9e6d9e4d4390ef6e037
bailey-martin <44448861+bailey-martin@users.noreply.github.com>
2018-12-07 17:15:14 -0500

    Our Many Changes (too many to list)

--- CSC360FinalProject/src/Interface/SocketTest.java
+++ CSC360FinalProject/src/Interface/SocketTest.java
@@ -45,18 +45,31 @@ public class SocketTest {
 
     public static void startSender() throws UnknownHostException { //beginning of startSender()
         //InetAddress aHost;
+        System.out.println ("Let's see what is stored in the Arraylist:");
+        for (int r = 0; r < ip_addresses.size(); r++){
+            System.out.println ("IP: " + ip_addresses.get(r));
+        }
         for (int i = 0; i < ip_addresses.size(); i++){
+            System.out.println ("HERE IS WHOM I WANT TO SEND TO: " + ip_addresses.get(i));
             InetAddress aHost = InetAddress.getByName(ip_addresses.get(i));
+            System.out.println ("HOST ADDRESS AMANDA TEST: " + aHost.getHostAddress());
+            //InetAddress aHost = InetAddress.getByName("10.18.40.48");
+            System.out.println ("HERE IS THE IP TO WHICH I AM TRYING TO SEND TO AT BEG OF START SENDER: " + ip_addresses.get(i));
+            System.out.println ("SIZE OF ARRAY LIST: " + ip_addresses.size());
             Scanner scan = new Scanner(System.in);
+            
             (new Thread() {
             @Override
             public void run() {  
+                                            System.out.println("THE THREAD HAS STARTED PEOPLE");
+
                 String stopLimit = "";
                 while (!("-".equals(stopLimit))) {
                     System.out.println("Please enter the news item that you wish to share, Enter '-' to escape input feed:\n");
                     String str = scan.nextLine();
+                    outerloop:
                     if(str.equals("-"))
-                        System.exit(0);
+                        break outerloop;
                     stopLimit = str;
                     byte data[] = str.getBytes();
                     DatagramSocket socket = null;
@@ -70,7 +83,9 @@ public class SocketTest {
                     data = null;
                     int q = 0;
                     while (q < 1) { //begin of while
+                        
                         try { //begin of try()
+                            System.out.println ("HERE IS WHOM I WANT TO SEND TO IN WHILE: " + aHost.toString());
                             System.out.println("Sending news item: " + new String(packet.getData()));
                             socket.send(packet);
                             Thread.sleep(50);
@@ -78,19 +93,23 @@ public class SocketTest {
                             System.out.println("Sending Attempt Number of News Item: " + q);
                         }//end of try
                         catch (IOException | InterruptedException ex) {
-                            ex.printStackTrace();
+                            System.out.println ("NAK-sending end");
                         }//end of catch()
                     }//end of while-loop
+                    System.out.println ("End of while loop with abreak");
                 }//end of while-loop
             }//end of run()
         }).start(); //end of thread
+            System.out.println ("END OF THE THREAD FIR SENDER!!!!!!!*@#&*(#&*#&*#&*(#&*");
         }//end of for-loop
+        System.out.println ("ENTERING THE FOR LOOP");
     } //end of startSender()
 
     public static void startServer() { //beginning of startServer()
         (new Thread() {
             @Override
             public void run() {
+                System.out.println ("THE BEGINNING OF THE RECVR THREAD");
                 DatagramSocket socket = null;
                 try {
                     socket = new DatagramSocket(55555); //listens on port 55555
@@ -100,7 +119,7 @@ public class SocketTest {
                 }//end of catch()
                 DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);//makes a new packet
                 String temp;
-                while (true) {
+                while (true) { 
                     try {
                         socket.receive(packet);
                         //temp = new String(packet.getData()); fixes overwrite issue
@@ -109,19 +128,29 @@ public class SocketTest {
                         
                         //break it up
                         String [] splitIPs = temp.split("/");
+                        for (int e = 0; e < splitIPs.length; e++){
+                            System.out.println ("HERE IS THE RAW SPLIT: " + splitIPs[e]);
+                        }
                         for (String s:splitIPs){
                             if (s.contains("1")){
                                 for (int i = 0; i < ip_addresses.size(); i++){
                                     if (ip_addresses.indexOf(s)==-1){
                                         ip_addresses.add(s);
-                                        //break;
+                                        System.out.println ("ADDING THE IP: " + s + " into the arrayList");
+                                        
+                                        break;
                                     }//end of if-statement
+                                    System.out.println ("End of if after IP add");
                                 }//end of for-loop
+                                System.out.println ("End of for after IP add");
                             }//end of if-statement to see if the message contains a valid IP number
+                            System.out.println ("End of if after IP add valid ip contains a 1");
                         }//end of for-each loop
+                        System.out.println ("End of for each loop");
                         
                         for (int q = 0; q < ip_addresses.size(); q++){
                             System.out.println ("ARRAYLIST IPS  " + ip_addresses.get(q));
+                            System.out.println ("Size == " + ip_addresses.size());
                         }//end of for-loop
                         
                         
@@ -133,6 +162,7 @@ public class SocketTest {
                 }//end of while
             }//end of run
         }).start(); //end of thread
+        System.out.println ("THE END OF THE RECVR THREAD");
     }//end of start server
 
     
